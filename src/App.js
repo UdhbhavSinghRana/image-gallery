d44a9cc5a4adf6e4d7ca0b55c884f45dba3fb269
Udhbhav Singh Rana <21889udbhav@gmail.com>
2023-02-24 19:33:12 +0530

    This commit for all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
+import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch'
+
 import './App.css';
 
 function App() {
+  const [images,setImages] = useState([]);
+  const [isLoading,setLoading] = useState(true);
+  const [term,setTerm] = useState('');
+  useEffect(() => {
+    console.log(process.env.REACT_APP_PIXABAY_API_KEY);
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+      .then(res => res.json())
+      .then(data => {
+        setImages(data.hits);
+        setLoading(false);
+      })
+      .catch(err => console.log(err));
+  }, [term]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mx-auto'>
+      <ImageSearch searchText ={(text) => setTerm(text)}/>
+      <div className='grid grid-cols-3'>
+        {images.map(image => (
+          <ImageCard key={image.id} image={image}/>
+        ))}
+      </div>
     </div>
+
   );
 }
 
